4bbfcd13019c1ddb9a0ea73bdaa5f42ed64fc5db
rgomeze96 <rgomeze@eafit.edu.co>
2021-10-27 12:42:13 -0500

    ready for taller 2 entrega

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,22 +3,32 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class Controller extends BaseController
 {
-    public static $quotes = array("The Black Knight Always Triumphs! - Monty Python",
-        "Anyone who has never made a mistake has never tried anything new - Albert Einstein",
-        "Never Stop Exploring - The North Face",
-        "Be yourself; everyone else is already taken - Oscar Wilde",
-        "So many books, so little time - Frank Zappa",
-        "Be the change that you wish to see in the world - Mahatma Gandhi",
+    public static $images = array("https://storage.googleapis.com/taller2-images/image1.png",
+        "https://storage.googleapis.com/taller2-images/image2.png",
+        "https://storage.googleapis.com/taller2-images/image3.png",
+        "https://storage.googleapis.com/taller2-images/image4.png",
+        "https://storage.googleapis.com/taller2-images/image5.png",
+        "https://storage.googleapis.com/taller2-images/image6.png",
+        "https://storage.googleapis.com/taller2-images/image7.png",
+        "https://storage.googleapis.com/taller2-images/image8.png",
+        "https://storage.googleapis.com/taller2-images/image9.png",
+        "https://storage.googleapis.com/taller2-images/image10.png",
+        "https://storage.googleapis.com/taller2-images/image11.png",
+        "https://storage.googleapis.com/taller2-images/image12.png",
+        "https://storage.googleapis.com/taller2-images/image13.png",
+        "https://storage.googleapis.com/taller2-images/image14.png",
+        "https://storage.googleapis.com/taller2-images/image15.png",
     );
 
     public function index()
     {
-        $totalQuotes = (count(Controller::$quotes));
-        $randomNumber = (rand(0, ($totalQuotes-1)));
-        $randomQuote = Controller::$quotes[$randomNumber];
-        return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
+        $numberOfImages = 15;
+        $randomNumber = (rand(0, ($numberOfImages-1)));
+        $randomImage = Controller::$images[$randomNumber];
+        return response("<img src=\"".$randomImage."\"> server_ip = ".gethostbyname((gethostname()))) ;
     }
 }
